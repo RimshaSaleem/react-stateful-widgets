894809a33b023caae04faf155712116357745c05
RimshaSaleem <rimshatif8@gmail.com>
2021-07-12 19:29:10 -0700

    wip

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -35,6 +35,7 @@ const [ids, setIds]= useState(null)
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
+    return programmerList[ids-1].name
   };
 
   const style = {
@@ -53,7 +54,7 @@ const [ids, setIds]= useState(null)
           we could never add or edit programmers in the future. The list would be a static thing." */
           listOfAwesome.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => { /* in here set the featured id to be dev.id */ }}>Feature</button>
+              {dev.name} <button onClick={() => setIds(dev.id)} >Feature</button>
             </div>
           )
         }
@@ -63,7 +64,7 @@ const [ids, setIds]= useState(null)
           // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
           // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
           // Replace the hard-coded false with the correct variable.
-          false
+          ids
             ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
             : 'Pick an awesome programmer'
         }
